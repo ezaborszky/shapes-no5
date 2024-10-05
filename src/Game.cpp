99500d015e1fa7f5dd35371033010de4e4f0dce5
ezaborszky <ede.zaborszky@gmail.com>
2024-10-05 19:24:07 +0200

    x#	modified:   src/CMakeLists.txt x#	modified:   src/Game.cpp x#	modified:   src/Game.h x#	src/Components.h x#	src/Entity.cpp  #	src/Entity.h

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,13 @@
 #include "Game.h"
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <iostream>
 #include <imgui-SFML.h>
 #include <imgui.h>
+#include <memory>
+#include "EntityManager.h"
 
 Game::Game()
 {
@@ -13,8 +16,19 @@ Game::Game()
   ImGui::SFML::Init(m_window);
 }
 
-void Game::run()
+void Game::sRender(sf::RenderWindow &window)
 {
+  for(auto a : entityManager.getEntitites())
+  {
+    window.draw(a->cShape->circle);
+  }
+
+}
+
+void Game::run()
+{ 
+
+  int pos = 10;
   sf::Clock deltaClock;
   while(m_window.isOpen())
   {
@@ -27,11 +41,25 @@ void Game::run()
       {
         m_window.close();
       }
+
+      if(event.type == sf::Event::KeyPressed)
+      {
+        std::cout << "asd " << std::endl;
+        auto a = entityManager.addEntity("alma");
+        a->name = "korte";
+        a->cShape = std::make_shared<CShape>(15,20);
+        a->cShape->circle.setPosition(pos,pos);
+        a->cShape->circle.setFillColor(sf::Color(255,100,255));
+        pos += 10; 
+          
+
+      }
     }
     
     ImGui::SFML::Update(m_window, deltaClock.restart());
     ImGui::ShowDemoWindow();
     m_window.clear();
+    sRender(m_window);
     ImGui::SFML::Render(m_window);
     m_window.display();
   } 
--- src/Game.h
+++ src/Game.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include "EntityManager.h"
 class Game
 {
   sf::RenderWindow m_window;
 
   public:
-
+  
+  EntityManager entityManager;
   Game();
   void run();
+  void sRender(sf::RenderWindow &window);
 };
