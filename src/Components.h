989425a4cc8e0b60e1a539c66e40fb6b1fbb103e
ezaborszky <ede.zaborszky@gmail.com>
2024-10-07 20:16:39 +0200

    update with vec2

--- src/Components.h
+++ src/Components.h
@@ -1,6 +1,17 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
+#include "Vec2.h"
+
+class CTransform
+{
+  Vec2 pos = {0.0, 0.0};
+  Vec2 speed = {0.0, 0.0};
+  int rotation = 0;
+
+  CTransform(Vec2 pos, Vec2 speed, int rotation)
+    :pos(pos), speed(speed), rotation(rotation){};
+};
 
 class CShape
 {
@@ -13,9 +24,9 @@ class CShape
     }
 };
 
-class CTransform
-{
-  public:
-    int x;
-    int y;
-};
+
+
+
+
+
+
