10fbd16f396630cc05f6ed160f5cf8d26113749c
ezaborszky <ede.zaborszky@gmail.com>
2024-10-07 19:23:24 +0200

    custom vec2

---
+++ src/Vec2.cpp
@@ -0,0 +1,59 @@
+#include <math.h>
+#include "Vec2.h"
+
+Vec2::Vec2(float xin, float yin)
+    : x(xin)
+    , y(yin)
+    {
+
+    }
+
+    Vec2 Vec2::operator + (const Vec2& rhs) const
+    {
+        return Vec2(x+rhs.x, y+rhs.y);
+    }
+
+    Vec2 Vec2::operator * (const float val) const
+    {
+        return Vec2(x*val, y*val);
+    }
+
+    Vec2 Vec2::operator - (const Vec2& rhs) const
+    {
+        return Vec2(x - rhs.x, y - rhs.y);
+    }
+
+    Vec2& Vec2::scale(float s)
+    {
+        x *= s;
+        y *= s;
+
+        return *this;
+    }
+
+    float Vec2::dist(Vec2 v)
+    {
+        float dx = v.x - x;
+        float dy = v.y - y;
+        return sqrtf((dx*dx) + (dy*dy));
+    }
+
+    float Vec2::length()
+    {
+        return sqrtf((x*x) + (y*y));
+    }
+
+    bool Vec2::operator == (Vec2 v)
+    {
+        return (x == v.x) && (y == v.y);
+    }
+
+
+
+
+
+
+
+
+
+
---
+++ src/Vec2.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Vec2
+{
+  public:
+    float x = 0;
+    float y = 0;
+
+    Vec2(){};
+    Vec2(float xin, float yin);
+
+    bool operator ==(const Vec2& rhs) const;
+    Vec2 operator +(const Vec2&rhs) const; 
+    Vec2 operator *(const float val) const;
+    Vec2 operator -(const Vec2& rhs) const;
+    Vec2& scale(float s); 
+    float dist(Vec2 v);
+    float length();
+    bool operator ==(Vec2 v);
+};
+
